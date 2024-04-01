3a2b9d8e9d518df47b40792386582e8a7bf04584
AttiqaNazir <atiqanazir15@gmail.com>
2024-04-01 23:26:29 +0200

    add angular material icons and buttons for controls

--- src/app/audio/audio.module.ts
+++ src/app/audio/audio.module.ts
@@ -3,10 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { AudioRoutingModule } from './audio-routing.module';
 import { AudioPlayerComponent } from './components/audio-player/audio-player.component';
-import {MatProgressBarModule} from "@angular/material/progress-bar";
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [AudioPlayerComponent],
-  imports: [CommonModule, AudioRoutingModule, MatProgressBarModule],
+  imports: [
+    CommonModule,
+    AudioRoutingModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+  ],
 })
 export class AudioModule {}
