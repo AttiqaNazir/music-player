4debe34c7edd61a91e37b364cde92195308aac11
AttiqaNazir <atiqanazir15@gmail.com>
2024-04-01 23:26:50 +0200

    update mock data

--- src/app/audio/fixtures/tracks.ts
+++ src/app/audio/fixtures/tracks.ts
@@ -1,22 +1,28 @@
-import {Track} from "../types/domain";
+import { Track } from '../types/domain';
 
 export const tracks: Track[] = [
   {
     url: 'https://ia801504.us.archive.org/3/items/EdSheeranPerfectOfficialMusicVideoListenVid.com/Ed_Sheeran_-_Perfect_Official_Music_Video%5BListenVid.com%5D.mp3',
     songName: 'Perfect',
     artistName: 'Ed Sheeran',
-    coverArt: "assets/Ed-Sheeran.jpg",
+    coverArt: 'assets/Ed-Sheeran.jpg',
   },
   {
     url: 'https://ia801609.us.archive.org/16/items/nusratcollection_20170414_0953/Man%20Atkiya%20Beparwah%20De%20Naal%20Nusrat%20Fateh%20Ali%20Khan.mp3',
     songName: 'Man Atkeya Beparwah',
     artistName: 'Nusrat Fateh Ali Khan',
-    coverArt: "assets/Ed-Sheeran.jpg",
+    coverArt: 'assets/Nusrat.jpg',
   },
   {
     url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
     songName: 'The never written role playing game',
     artistName: 'Kangaroo MusiQue',
-    coverArt: "assets/Ed-Sheeran.jpg",
+    coverArt: 'assets/Ed-Sheeran.jpg',
+  },
+  {
+    url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-12.mp3',
+    songName: 'Sample 3',
+    artistName: 'Artist 3',
+    coverArt: 'https://i1.sndcdn.com/artworks-000249294066-uow7s0-t500x500.jpg',
   },
 ];
--- src/app/audio/types/domain.ts
+++ src/app/audio/types/domain.ts
@@ -1,6 +1,6 @@
 export type Track = {
-  songName: string
-  url: string
-  artistName: string
-  coverArt: string
+  songName: string;
+  url: string;
+  artistName: string;
+  coverArt: string;
 };
