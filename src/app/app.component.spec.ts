e2616600308aa415018a76a665f670ffd84d2ae9
AttiqaNazir <atiqanazir15@gmail.com>
2024-04-03 10:45:10 +0200

    fix unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('app-music-player');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'app-music-player app is running!',
-    );
-  });
 });
--- src/app/audio/components/audio-player/audio-player.component.spec.ts
+++ src/app/audio/components/audio-player/audio-player.component.spec.ts
@@ -1,13 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AudioPlayerComponent } from './audio-player.component';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 describe('AudioPlayerComponent', () => {
   let component: AudioPlayerComponent;
   let fixture: ComponentFixture<AudioPlayerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        MatProgressBarModule,
+        MatCardModule,
+        MatIconModule,
+        MatListModule,
+      ],
       declarations: [AudioPlayerComponent],
     }).compileComponents();
 
